651cf838cf6afe2db7f725d9b9940b8459da5dff
johnhomelab <john.andrade.fsa@gmail.com>
2026-02-01 22:58:01 -0300

    Add Media collection configuration

---
+++ src/collections/Media.ts
@@ -0,0 +1,17 @@
+  GNU nano 7.2                                                            Media.ts                                                                      
+import type { CollectionConfig } from 'payload'
+
+export const Media: CollectionConfig = {
+  slug: 'media',
+  access: {
+    read: () => true,
+  },
+  fields: [
+    {
+      name: 'alt',
+      type: 'text',
+      required: true,
+    },
+  ],
+  upload: true,
+}
