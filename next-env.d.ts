631ea109142c22ef1a965b91660138ceb3308a52
johnhomelab <john.andrade.fsa@gmail.com>
2026-02-01 22:36:36 -0300

    Create next-env.d.ts

---
+++ next-env.d.ts
@@ -0,0 +1,6 @@
+  GNU nano 7.2                                                          next-env.d.ts                                                                   
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/app/building-your-application/configuring/typescript for more information.
